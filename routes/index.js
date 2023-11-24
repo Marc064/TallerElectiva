db21d4709042ebe2abe796d935afbde7730b3680
Marc064 <95501065+Marc064@users.noreply.github.com>
2023-11-24 10:44:56 -0500

    Adaptado json

--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,19 @@ router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
 
 const filePath = path.join(__dirname, '../public/datos.json')
+let jsonMap = ""
 
 fs.readFile(filePath, 'utf8', (err, data) => {
     try {
         const jsonData = JSON.parse(data)
-        const jsonMap = new Map(Object.entries(jsonData))
+        jsonMap = new Map(Object.entries(jsonData))
         console.log(jsonMap)
     } catch (parseError) {
         console.error('Error al parsear el archivo JSON', parseError)
     }
 })
 
+console.log(jsonMap);
 
 router.get('/', (req, res) => res.render('index', { title: 'Taller electiva' }))
 
