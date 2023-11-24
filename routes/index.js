c5864da8fd53210a4c84c6bcb2c68e2c5a11700d
Marc064 <95501065+Marc064@users.noreply.github.com>
2023-11-24 11:09:04 -0500

    reconstitucion de json

--- routes/index.js
+++ routes/index.js
@@ -6,21 +6,22 @@ const fs = require("fs")
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
 
-const filePath = path.join(__dirname, '../public/datos.json')
-let jsonMap = ""
+const filePath = path.join(__dirname, '../public/products.json')
+let products = ""
 
 fs.readFile(filePath, 'utf8', (err, data) => {
     try {
         const jsonData = JSON.parse(data)
-        jsonMap = new Map(Object.entries(jsonData))
-        console.log(jsonMap)
+        products = new Map(Object.entries(jsonData))
+        console.log(products)
     } catch (parseError) {
         console.error('Error al parsear el archivo JSON', parseError)
     }
 })
 
-console.log(jsonMap);
-
+console.log(products);
+products = "Si"
 router.get('/', (req, res) => res.render('index', { title: 'Taller electiva' }))
+router.get('/products', (req, res) => res.render('products', { title : 'Productos', product: products}))
 
 module.exports = router
