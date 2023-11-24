619aa0ffaf4c2dc31b915dd28e9d92cc9619e9e1
Marc064 <95501065+Marc064@users.noreply.github.com>
2023-11-24 10:32:06 -0500

    Lectura archivo json

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const router = express.Router()
 const path = require("path")
-
+const fs = require("fs")
 
 router.use(express.json())
 router.use(express.urlencoded({ extended: true }))
 
-router.get('/', (req, res) => res.render('index', { title: 'Taller electiva'}))
+const filePath = path.join(__dirname, '../public/datos.json')
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+    try {
+        const jsonData = JSON.parse(data)
+        const jsonMap = new Map(Object.entries(jsonData))
+        console.log(jsonMap)
+    } catch (parseError) {
+        console.error('Error al parsear el archivo JSON', parseError)
+    }
+})
+
+
+router.get('/', (req, res) => res.render('index', { title: 'Taller electiva' }))
 
 module.exports = router
