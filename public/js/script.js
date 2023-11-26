66e2c5bd47a850bdef33be3f884c32476cec836f
Marc064 <95501065+Marc064@users.noreply.github.com>
2023-11-25 21:49:10 -0500

    Productos y proveedores activos

---
+++ public/js/script.js
@@ -0,0 +1,52 @@
+function saveProduct() {
+    const id = document.getElementById('idProduct').value;
+    const name = document.getElementById('nameProduct').value;
+    const price = document.getElementById('priceProduct').value;
+    const stock = document.getElementById('stockProduct').value;
+
+    if (id !== "" && name !== "" && price !== "" && stock !== "") {
+
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", "/products", true);
+        xhr.onreadystatechange = () => {
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState == 4 && xhr.status == 200) {
+                    
+                }
+            }
+        }
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+        const data = `id=${encodeURIComponent(id)}&name=${encodeURIComponent(name)}&price=${encodeURIComponent(price)}&stock=${encodeURIComponent(stock)}`;
+
+        xhr.send(data);
+        window.location.reload();
+    } else {
+        alert('Se deben diligenciar todos los campos');
+    }
+}
+
+function saveSupplier() {
+    const id = document.getElementById('idSupplier').value;
+    const name = document.getElementById('nameSupplier').value;
+    const number = document.getElementById('numberProduct').value;
+    const email = document.getElementById('emailSupplier').value;
+
+    if (id !== "" && name !== "" && number !== "" && email !== "") {
+
+        const xhr = new XMLHttpRequest()
+        xhr.open("POST", "/suppliers", true)
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+            }
+        }
+        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+        
+        const data = `id=${encodeURIComponent(id)}&name=${encodeURIComponent(name)}&number=${encodeURIComponent(number)}&email=${encodeURIComponent(email)}`;
+
+        xhr.send(data);
+        window.location.reload();
+    } else {
+        alert('Se deben diligenciar todos los campos');
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -29,13 +29,13 @@ for (let i = 0; i < filePaths.length; i++) {
     try {
         const data = fs.readFileSync(filePath, 'utf8');
         
-        // Verificar si la cadena JSON no está vacía antes de intentar analizarla
         if (data.trim() !== '') {
             const jsonData = JSON.parse(data);
             fileNames[i] = new Map(Object.entries(jsonData));
             console.log(fileNames[i]);
         } else {
             console.log(`El archivo ${filePath} está vacío.`);
+            console.log(fileNames[i]);
         }
     } catch (parseError) {
         console.error(`Error al parsear el archivo JSON ${filePath}:`, parseError);
@@ -48,48 +48,89 @@ const exists = (id, fileMap) => Array.from(fileMap.values()).some(obj => obj.id 
 
 const update = (fileIndex, fileMap) => {
     try {
-        const jsonData = JSON.stringify(Array.from(fileMap.values()));
-        fs.writeFileSync(filePaths[fileIndex], jsonData, 'utf8');
-        const index = filePaths.indexOf(filePaths[fileIndex]);
+        const jsonData = JSON.stringify(Array.from(fileMap.values()))
+        fs.writeFileSync(filePaths[fileIndex], jsonData, 'utf8')
+        const index = filePaths.indexOf(filePaths[fileIndex])
         if (index !== -1) {
-            fileNames[index] = new Map(fileMap);
+            fileNames[index] = new Map(fileMap)
         }
+        console.log(fileNames[fileIndex])
     } catch (error) {
-        console.error(`Error al actualizar el archivo: ${filePaths[fileIndex]}`, error);
+        console.error(`Error al actualizar el archivo: ${filePaths[fileIndex]}`, error)
     }
 }
 
 
-fileNames[0].set('34213', {'dhdhdhd': 4323});
-fileNames[0].set('63446', {'dhdhdhd': 'dsfsdf'});
-update(0, fileNames[0]);
-console.log(fileNames[0]);
+
 
 router.get('/', (req, res) => res.render('index', { title: 'Inicio' }))
 router.get('/products', (req, res) => res.render('products', { title: 'Productos', product: fileNames[0] }))
 router.get('/sales', (req, res) => res.render('sales', { title: 'Facturacion', sale: fileNames[1] }))
-router.get('/counts', (req, res) => res.render('count', { title: 'Cuentas', count: fileNames[2] }))
-router.get('/suppliers', (req, res) => res.render('suppliers', { title: 'Proveedores', supplier: fileNames[3] }))
+router.get('/suppliers', (req, res) => res.render('suppliers', { title: 'Proveedores', supplier: fileNames[2] }))
+router.get('/counts', (req, res) => res.render('count', { title: 'Cuentas', count: fileNames[3] }))
 
 router.post('/products', (req, res) => {
     const { id, name, price, stock } = req.body;
-
     if (!exists(id, fileNames[0])) {
-        const newProductId = fileNames[0].size + 1;
+        const newProductId = fileNames[0].size;
         fileNames[0].set(newProductId, {
             id: id,
             name: name,
             price: price,
             stock: stock
         });
-        update(filePaths[0], fileNames[0])
+        
+        if (filePaths[0]) {
+            update(0, fileNames[0]);
+        } else {
+            console.error('Error: filePaths[0] no está definido.');
+        }
     } else {
+
+        const existingProduct = Array.from(fileNames[0].values()).find(product => product.id === id)
         existingProduct.name = name
         existingProduct.price = price
         existingProduct.stock = stock
-        update(filePaths[0], fileNames[0])
+
+        if (filePaths[0]) {
+            update(0, fileNames[0])
+        } else {
+            console.error('Error: filePaths[0] no está definido.')
+        }
     }
-});
+})
+
+router.post('/suppliers', (req, res) => {
+    const { id, name, number, email } = req.body;
+    if (!exists(id, fileNames[2])) {
+        const newProductId = fileNames[2].size;
+        fileNames[2].set(newProductId, {
+            id: id,
+            name: name,
+            number: number,
+            email: email
+        });
+        
+        if (filePaths[2]) {
+            update(2, fileNames[2]);
+        } else {
+            console.error('Error: filePaths[2] no está definido.');
+        }
+    } else {
+
+        const existingProduct = Array.from(fileNames[2].values()).find(product => product.id === id)
+        existingProduct.name = name
+        existingProduct.number = number
+        existingProduct.email = email
+
+        if (filePaths[2]) {
+            update(2, fileNames[2])
+        } else {
+            console.error('Error: filePaths[2] no está definido.')
+        }
+    }
+})
+
 
 
 
