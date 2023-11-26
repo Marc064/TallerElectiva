f7df82e396064a31eb6e17bedb613211b7ad5ffe
Marc064 <95501065+Marc064@users.noreply.github.com>
2023-11-25 19:22:48 -0500

    Creacion de json y actualizacion de json dependiendo de los cambios

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,94 @@
-const express = require("express")
-const router = express.Router()
-const path = require("path")
-const fs = require("fs")
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const fs = require("fs");
 
-router.use(express.json())
-router.use(express.urlencoded({ extended: true }))
+router.use(express.json());
+router.use(express.urlencoded({ extended: true }));
 
-const filePath = path.join(__dirname, '../../Persistencia/products.json')
-let products = ""
+const filePaths = [
+    path.join(__dirname, "../../products.json"),
+    path.join(__dirname, "../../sales.json"),
+    path.join(__dirname, "../../suppliers.json"),
+    path.join(__dirname, "../../count.json")
+];
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-    try {
-        const jsonData = JSON.parse(data)
-        products = new Map(Object.entries(jsonData))
-        console.log(products)
-    } catch (parseError) {
-        console.error('Error al parsear el archivo JSON', parseError)
+let products = new Map();
+let sales = new Map();
+let suppliers = new Map();
+let counts = new Map();
+
+const fileNames = [products, sales, suppliers, counts];
+
+for (let i = 0; i < filePaths.length; i++) {
+    const filePath = filePaths[i]
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, '[]', 'utf8');
     }
-})
 
-console.log(products);
-products = "Si"
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        
+        // Verificar si la cadena JSON no está vacía antes de intentar analizarla
+        if (data.trim() !== '') {
+            const jsonData = JSON.parse(data);
+            fileNames[i] = new Map(Object.entries(jsonData));
+            console.log(fileNames[i]);
+        } else {
+            console.log(`El archivo ${filePath} está vacío.`);
+        }
+    } catch (parseError) {
+        console.error(`Error al parsear el archivo JSON ${filePath}:`, parseError);
+    }
+    
+}
+
+const update = (fileIndex, fileMap) => {
+    try {
+        const jsonData = JSON.stringify(Array.from(fileMap.values()));
+        fs.writeFileSync(filePaths[fileIndex], jsonData, 'utf8');
+        const index = filePaths.indexOf(filePaths[fileIndex]);
+        if (index !== -1) {
+            fileNames[index] = new Map(fileMap);
+        }
+    } catch (error) {
+        console.error(`Error al actualizar el archivo: ${filePaths[fileIndex]}`, error);
+    }
+}
+
+
+fileNames[0].set('34213', {'dhdhdhd': 4323});
+fileNames[0].set('63446', {'dhdhdhd': 'dsfsdf'});
+update(0, fileNames[0]);
+console.log(fileNames[0]);
+
 router.get('/', (req, res) => res.render('index', { title: 'Inicio' }))
-router.get('/products', (req, res) => res.render('products', { title : 'Productos', product: products}))
-router.get('/sales', (req, res) => res.render('sales', {title:'Facturacion'}))
-router.get('/users', (req, res) => res.render('users', {title:'Usuarios'}))
-router.get('/suppliers', (req, res) => res.render('suppliers', {title:'Proveedores'}))
+router.get('/products', (req, res) => res.render('products', { title: 'Productos', product: fileNames[0] }))
+router.get('/sales', (req, res) => res.render('sales', { title: 'Facturacion', sale: fileNames[1] }))
+router.get('/counts', (req, res) => res.render('count', { title: 'Cuentas', count: fileNames[2] }))
+router.get('/suppliers', (req, res) => res.render('suppliers', { title: 'Proveedores', supplier: fileNames[3] }))
 
-module.exports = router
+router.post('/products', (req, res) => {
+    const { id, name, price, stock } = req.body;
+
+    if (!existsProduct(id)) {
+        const newProductId = fileNames[0].size + 1;
+        fileNames[0].set(newProductId, {
+            id: id,
+            name: name,
+            price: price,
+            stock: stock
+        });
+        update(filePaths[0], fileNames[0])
+    } else {
+        existingProduct.name = name
+        existingProduct.price = price
+        existingProduct.stock = stock
+        update(filePaths[0], fileNames[0])
+    }
+});
+
+
+
+
+module.exports = router;
